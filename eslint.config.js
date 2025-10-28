fcb0a3be967fca76469b4cdba72a4e01cd955959
Ali Kayani <akayanigithub@outlook.com>
2025-10-28 08:40:41 +0000

    Add ESLint linting configuration

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        alert: 'readonly',
+        setTimeout: 'readonly',
+        requestAnimationFrame: 'readonly',
+        MouseEvent: 'readonly',
+        HTMLDivElement: 'readonly',
+        HTMLButtonElement: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+];
